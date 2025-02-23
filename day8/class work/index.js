f1b973b4865178d846e2767f7f2b5a717cad671a
rimpa0019 <r.daripa02@gmail.com>
2025-02-23 21:22:30 +0530

    day8

---
+++ day8/class work/index.js
@@ -0,0 +1,30 @@
+document.getElementById("parent").style.border="1px solid black"
+
+document.getElementById("parent").style.height="200px";
+
+document.getElementById("parent").style.backgroundColor="blue";
+
+let element = document.createElement("button")
+
+element.innerText="submit";
+
+element.style.backgroundColor="red"
+
+element.style.padding="10px"
+
+document.getElementById("parent").append(element)
+
+document.getElementById("parent").append(element)
+
+
+
+console.log(variabe)
+
+
+
+function myfunction (){
+    var variable = 10
+    console.log(variable)
+}
+
+myfunction()
