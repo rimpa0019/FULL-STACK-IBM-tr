63cd3480dfef51b6a2ffb7ad40103744d82094ad
rimpa0019 <r.daripa02@gmail.com>
2025-02-24 22:57:23 +0530

    day9

---
+++ day10/assingment/index.js
@@ -0,0 +1,18 @@
+let arr = [1,2,3,"rimi",1,2,true,false]
+
+console.log(arr[3]);
+
+// arr[3] = "rimu";
+console.log(arr[3]);
+console.log(arr);
+console.log(arr[arr.lengh-1])
+arr.push("svu");
+console.log(arr);
+arr[4]="rimu";
+arr.pop();
+console.log(arr);
+
+
+
+
+
