66a562973ee8105057855003b69796f713db847d
rimpa0019 <r.daripa02@gmail.com>
2025-03-07 12:52:48 +0530

    day18

---
+++ day18/classwork/script.js
@@ -0,0 +1,40 @@
+async function getData(){
+    let API_KEY = "AIzaSyBYfSC1XdqO7QRWPDAMLMipQyJaiaDJRhY"
+    let search_term= (document.getElementById ('query').value ||"univercity"
+    let data=fetchhttps:https://youtube.googleapis.com/youtube/v3/search?part=snippet&order=rating&q=${search_term}&type=video&videoCaption=any&videoDefinition=any&videoEmbeddable=true&videoLicense=any&maxResults=20&videoType=any&key=${API_KEY}
+)
+let response = await data.json()
+console.log.(data);
+console.log(response.item);
+} catch (error){
+
+}
+
+getData()
+{/{/*<iframe width="884" height="497" src="https://www.youtube.com/embed/qUFyj6mMCZ0" title="INDIA VS AUSTRALIA ODI FINAL MATCH FULL MATCH HIGHLIGHTS | IND VS AUS MOST THRILLING EVER🔥😱" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>*/
+}*<iframe width="884" height="497" src="https://www.youtube.com/embed/qUFyj6mMCZ0" title="INDIA VS AUSTRALIA ODI FINAL MATCH FULL MATCH HIGHLIGHTS | IND VS AUS MOST THRILLING EVER🔥😱" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>*/
+}
+
+async function showidcard(){
+    Array.array.array.forEach(element => {
+        let videoBox = document.createElement('div')
+        videoBox.className = "videos";
+
+        let img = document.createElement('img')
+        img.src = "";
+
+        let title = document.createElement('p')
+
+        uper
+
+
+
+        
+    });
+        
+    });
+}
+
+
+
+
---
+++ day18/classwork/video.js
@@ -0,0 +1,12 @@
+let videocontaier = document.getElementById("video");
+
+
+let data = JSON.parse(localStorage.getItem(videoData));
+
+
+async function showData(){
+    let box = document.getElement("div");
+
+    let iframe = document.createElement("iframe");
+    iframe.src = 
+}
